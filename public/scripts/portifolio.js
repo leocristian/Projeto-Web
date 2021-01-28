a92176fbabd882dc88f19c763be2822e620f85b3
Rubenilson de Sousa Lima <rubenilsondesousa@gmail.com>
2021-01-28 15:37:20 -0300

    portifolio text

--- public/scripts/portifolio.js
+++ public/scripts/portifolio.js
@@ -1,9 +1,10 @@
 function selectImage(event){
     const button = event.currentTarget
-    
+    console.log(button.children)
     //remove todos os active
     const buttons = document.querySelectorAll(".images button")
     
+
     buttons.forEach(removeActionClass)
 
     function removeActionClass(button){
@@ -12,10 +13,18 @@ function selectImage(event){
 
     //selecionar a imagem
     const image = button.children[0]
+    const imageTXT = button.children[1]
+    const imageTXTinfo = button.children[2]
+
     const imageSelection = document.querySelector(".slide-inner > img")
+    const imageTXTSelection = document.querySelector(".slide-inner > h1")
+    const imageTXTinfoSelection = document.querySelector(".slide-inner > p")
+
 
     //atualizar o container imagem
     imageSelection.src = image.src
+    imageTXTSelection.textContent = imageTXT.textContent
+    imageTXTinfoSelection.textContent = imageTXTinfo.textContent
 
     //adicionar a classe .active a imagem
     button.classList.add("active")
